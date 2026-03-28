56b1e81ea341f78477cbd8707210f1b54c0ad9e7
Harshit Mahapatra <harshitmahapatra@gmail.com>
2026-03-28 16:48:01 +0100

    fix: update test expectations and bump version to 0.3.4
    Update complex_scripts.txt to use quoted_assignment_string for the
    set "VAR=value" form, fixing the failing test after the assignment_string
    split introduced in the previous commit.

--- src/parser.c
+++ src/parser.c
@@ -4006,7 +4006,7 @@ TS_PUBLIC const TSLanguage *tree_sitter_batch(void) {
     .metadata = {
       .major_version = 0,
       .minor_version = 3,
-      .patch_version = 3,
+      .patch_version = 4,
     },
   };
   return &language;
